880c576f0c29e9329bfa659dad04e1f3a2cdaf6e
rssteffey <rssteffey@gmail.com>
2014-03-25 22:22:38 -0500

    Major Update #2.5 - Bug Fixes
    Fixing broken links and the like.  Email me if you find any glaring bugs
    or formatting issues.

---
+++ test1.js
@@ -0,0 +1,28 @@
+
+
+
+function draw()
+{
+  var canvas = document.getElementById('code');
+  if (canvas.getContext)
+  {
+	 var ctx = canvas.getContext('2d');
+  }
+  
+	var Grid = [][];
+	var Paths = [][];
+	var Temps = [];
+  
+	ctx.fillStyle = "black";
+	ctx.beginPath();
+	ctx.moveTo(125,125);
+	ctx.lineTo(125,45);
+	ctx.lineTo(45,125);
+	ctx.stroke();
+	 
+	for (var i = 0; i < arr.length; i++)
+	{
+		ctx.lineTo
+	}
+  
+}
